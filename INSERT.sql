c0a32432a982da0df72f7e2587f168c9eb4f53e9
Neukz <kacperneumann1@gmail.com>
2025-12-09 10:29:56 +0100

    Add inserts to producer table

---
+++ INSERT.sql
@@ -0,0 +1,13 @@
+INSERT INTO producer (name, partnership_start_date, partnership_end_date)
+VALUES
+	('Airbus', '1992-01-01', NULL),
+	('Boeing', '1994-07-01', NULL),
+	('Bombardier', '1997-10-01', NULL),
+	('Embraer', '1999-04-01', NULL),
+	('Gulfstream Aerospace', '2005-07-01', NULL),
+	('Mitsubishi Aircraft', '2012-01-01', '2030-12-31'),
+	('Sukhoi', '2008-01-01', '2027-10-31'),
+	('De Havilland Canada', '2004-04-01', '2027-06-30'),
+	('ATR', '1992-01-01', '2029-12-31'),
+	('COMAC', '2001-01-01', '2027-12-31');
+
